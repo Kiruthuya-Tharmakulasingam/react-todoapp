3c8bedb33d5232c5f58796313b7df2e5570380cc
Kiruthuya <kiruthuyakiruthu@gmail.com>
2025-10-21 22:41:56 +0530

    style added

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -19,22 +19,22 @@ function TodoApp() {
   }
   return (
     <>
-      <div>
+      <div style={styles.box}>
         <h1>My To-Do List</h1>
         <input
           type="text"
           placeholder="Enter a new task..."
           value={task}
-          onChange={(e) => setTask(e.target.value)}
+          onChange={(e) => setTask(e.target.value)} style={styles.input}
         />
-        <button onClick={addTask}>
+        <button style={styles.addBtn} onClick={addTask}>
           Add Task
         </button>
         <div>
           {todos.map((item, index) => (
-            <div key={index}>
+            <div key={index} style={styles.task}>
               <span>{item}</span>
-              <button onClick={() => deleteTask(index)}>
+              <button style={styles.delBtn} onClick={() => deleteTask(index)}>
                 Del
               </button>
             </div>
@@ -44,6 +44,51 @@ function TodoApp() {
     </>
   )
 }
+
+const styles = {
+  box: {
+    backgroundColor: '#f0f1f7ff',
+    width: '300px',
+    margin: '200px auto',
+    padding: '20px',
+    borderRadius: '10px',
+    boxShadow: '0 10px 10px rgba(0,0,0,0.1)',
+    textAlign: 'center',
+  },
+  input: {
+    padding: '8px',
+    width: '193px',
+    borderRadius: '5px',
+    border: '1px solid #ecd9d9ff',
+    marginRight: '8px',
+  },
+  addBtn: {
+    backgroundColor: '#007bff',
+    color: 'white',
+    border: 'none',
+    borderRadius: '5px',
+    padding: '8px 12px',
+    cursor: 'pointer',
+  },
+  task: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    margin: '8px 0',
+    padding: '6px 10px',
+    borderRadius: '6px',
+    boxShadow: '0 1px 3px rgba(0,0,0,0.1)',
+  },
+  delBtn: {
+    backgroundColor: 'transparent',
+    border: 'none',
+    color: 'black',
+    fontWeight: 'bold',
+    cursor: 'pointer',
+  },
+}
+
 
 export default TodoApp
 
